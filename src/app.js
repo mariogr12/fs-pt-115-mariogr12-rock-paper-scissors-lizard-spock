8650c8a71093d78c440f2b563d1c02dd773e22f3
mariogr12 <mariogrguitar12@gmail.com>
2025-06-20 11:45:59 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+let opcionesJuego = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+let reglas = {
+    Rock: ["Scissors", "Lizard"],
+    Paper: ["Rock","Spock"],
+    Scissors: ["Paper", "Lizard"],
+    Lizard: ["Paper", "Spock"],
+    Spock: ["Scissors", "Rock"]
+}
 
+const eleccionAutomatica = (opciones) => {
+    return opciones[Math.floor(Math.random()*opciones.length)]
+}
+
+let eleccionUsuario = prompt("Elige una de las siguientes opciones: Rock, Paper, Scissors, Lizard o Spock")
+eleccionUsuario=eleccionUsuario.charAt(0).toUpperCase()+eleccionUsuario.slice(1);
+
+let eleccionCpu = eleccionAutomatica(opcionesJuego)
+let jugadaActual = eleccionUsuario + eleccionCpu
+
+console.log("La cpu ha elegido: " + eleccionCpu);
+console.log("El usuario ha elegido: " + eleccionUsuario);
+
+if (eleccionUsuario===eleccionCpu) {
+    console.log("Empate!");
+} else {
+    if (reglas[eleccionUsuario].includes(eleccionCpu)) {
+        console.log(`Gana el usuario porque ${eleccionUsuario} gana a ${eleccionCpu}`);
+    } else{
+        console.log(`Gana la cpu porque ${eleccionCpu} gana a ${eleccionUsuario}`); 
+    }
+}
