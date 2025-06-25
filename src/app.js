29ebf4846e4f0271f1cbbffaaf3db76f57fffda0
mariogr12 <mariogrguitar12@gmail.com>
2025-06-25 18:35:10 +0000

    Anidación eliminada

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
 let opcionesJuego = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
 let reglas = {
     Rock: ["Scissors", "Lizard"],
-    Paper: ["Rock","Spock"],
+    Paper: ["Rock", "Spock"],
     Scissors: ["Paper", "Lizard"],
     Lizard: ["Paper", "Spock"],
     Spock: ["Scissors", "Rock"]
 }
 
 const eleccionAutomatica = (opciones) => {
-    return opciones[Math.floor(Math.random()*opciones.length)]
+    return opciones[Math.floor(Math.random() * opciones.length)]
 }
 
-let eleccionUsuario = prompt("Elige una de las siguientes opciones: Rock, Paper, Scissors, Lizard o Spock")
-eleccionUsuario=eleccionUsuario.charAt(0).toUpperCase()+eleccionUsuario.slice(1).toLowerCase();
+const jugadaActual = () => {
 
-let eleccionCpu = eleccionAutomatica(opcionesJuego)
-let jugadaActual = eleccionUsuario + eleccionCpu
+    let eleccionUsuario = prompt("Elige una de las siguientes opciones: Rock, Paper, Scissors, Lizard o Spock")
+    eleccionUsuario = eleccionUsuario.charAt(0).toUpperCase() + eleccionUsuario.slice(1).toLowerCase();
 
-console.log("La cpu ha elegido: " + eleccionCpu);
-console.log("El usuario ha elegido: " + eleccionUsuario);
+    let eleccionCpu = eleccionAutomatica(opcionesJuego)
 
-if (eleccionUsuario===eleccionCpu) {
-    console.log("Empate!");
-} else {
-    if (reglas[eleccionUsuario].includes(eleccionCpu)) {
-        console.log(`Gana el usuario porque ${eleccionUsuario} gana a ${eleccionCpu}`);
-    } else{
-        console.log(`Gana la cpu porque ${eleccionCpu} gana a ${eleccionUsuario}`); 
-    }
+    console.log("La cpu ha elegido: " + eleccionCpu);
+    console.log("El usuario ha elegido: " + eleccionUsuario);
+
+    if (eleccionUsuario === eleccionCpu)
+        return "Empate!"
+    if (reglas[eleccionUsuario].includes(eleccionCpu))
+        return `Gana el usuario porque ${eleccionUsuario} gana a ${eleccionCpu}`;
+    return `Gana la cpu porque ${eleccionCpu} gana a ${eleccionUsuario}`;
 }
+
+console.log(jugadaActual());
